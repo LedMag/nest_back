1f22cb34f0a37b643710de62ecf70f650019c9e6
LedMag <ledmaggazin@gmail.com>
2023-05-10 10:16:51 +0200

    feat: env variables

--- Dockerfile
+++ Dockerfile
@@ -16,14 +16,6 @@ FROM node:16-alpine3.16 as prod
 
 RUN mkdir -p /usr/src/app/
 
-# ENV NODE_ENV=${NODE_ENV}
-ENV PORT=9000
-ENV MYSQL_HOST=lu_db
-ENV MYSQL_PORT=3306
-ENV MYSQL_DATABASE=lusine
-ENV MYSQL_USERNAME=root
-ENV MYSQL_PASSWORD=root
-
 WORKDIR /usr/src/app/
 
 COPY package.json .
--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('config', () => {
     return {
+        BACKEND_PORT: process.env.BACKEND_PORT,
         MYSQL_HOST: process.env.MYSQL_HOST, 
         MYSQL_PORT: process.env.MYSQL_PORT, 
         MYSQL_DATABASE: process.env.MYSQL_DATABASE, 
